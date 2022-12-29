26fefcf952e0f638dc2b7ce1bc7f02bdb09b0dce
Bomi Choi <hibomi97@gmail.com>
2022-12-29 14:57:38 +0900

    Feat: 애니메이션 지속시간을 랜덤값으로 설정

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+const MIN_DURATION = 10;
 const body = document.querySelector("body");
 
 function makeSnowflake() {
     const snowflake = document.createElement("div");
     const delay = Math.random() * 10;
     const initialOpacity = Math.random();
+    const duration = Math.random() * 20 + MIN_DURATION;
 
     snowflake.classList.add("snowflake");
     snowflake.style.left = `${Math.random() * window.screen.width}px`;
     snowflake.style.animationDelay = `${delay}s`;
     snowflake.style.opacity = initialOpacity;
+    snowflake.style.animation = `fall ${duration}s linear`;
 
-    body.append(snowflake);
+    body.appendChild(snowflake);
 }
 
-makeSnowflake();
 for (let index = 0; index < 50; index++) {
     makeSnowflake();
 }
