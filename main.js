e02fa54ccef8a6e5e4d89d41a1385ef0204c2507
Bomi Choi <hibomi97@gmail.com>
2022-12-29 14:53:53 +0900

    Style: 눈송이 불투명도를 랜덤값으로 설정

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const body = document.querySelector("body");
 function makeSnowflake() {
     const snowflake = document.createElement("div");
     const delay = Math.random() * 10;
+    const initialOpacity = Math.random();
 
     snowflake.classList.add("snowflake");
     snowflake.style.left = `${Math.random() * window.screen.width}px`;
     snowflake.style.animationDelay = `${delay}s`;
+    snowflake.style.opacity = initialOpacity;
 
     body.append(snowflake);
 }
